28939a402f7b09fe468228c03588621c6d3a0b05
hajijoh13-art <hajijoh13-art@users.noreply.github.com>
2026-05-05 20:06:44 +0900

    Remove static export config for Vercel deployment

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  output: "export",
-  basePath: "/todo-nextjs-hajijo",
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
